45c099b842bc742a444612f2b19a144eb2d2d836
jepandsome <jepandsome@gmail.com>
2026-09-22 13:57:22 -0400

    feat: Set default filename if new file name is null

--- notebook/index.js
+++ notebook/index.js
@@ -222,6 +222,7 @@ function readFile(file) {
 
 function createNewFile() {
 	const filename = document.getElementById('new-file-name').value;
+	const filename = filename == null ? "default" : filename;
 	const id = 0;
 	const name = "First page (Use page tools to rename)";
 	const text = "This is a page with text";
