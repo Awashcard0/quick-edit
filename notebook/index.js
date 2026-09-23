16b081756f88b17446bd9ba92d0dfc6afd06470d
Awashcard0 <lumpy27@outlook.com>
2026-09-23 13:00:33 -0400

    feat: Enable button only when filename is provided for new notebook

--- notebook/index.js
+++ notebook/index.js
@@ -240,7 +240,6 @@ function readFile(file) {
 
 function createNewFile() {
 	let filename = document.getElementById('new-file-name').value;
-    filename = filename == null ? "default" : filename;
 	const id = 0;
 	const name = "First page (Use page tools to rename)";
 	const text = "This is a page with text";
